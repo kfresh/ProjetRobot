2a7ec3aa7e7990746231fd93ac5d23db3dbd8cd1
SAID Abdoul Kader <kdr_star@hotmail.com>
2014-01-03 19:36:09 +0100

    station

--- packages/fr/up10/miage/ProjetRobot/Station.java
+++ packages/fr/up10/miage/ProjetRobot/Station.java
@@ -2,10 +2,17 @@ package fr.up10.miage.ProjetRobot;
 
 public class Station extends Thread{
 	private Robot[] FiledAttente ;
+	private Robot RobotEncharge;
 	
 	public Station(){
 		Robot[] FiledAttente = new Robot[3];
 		
 	}
+	
+	public void run(){
+	
+	}
+	
+	
 	
 }
