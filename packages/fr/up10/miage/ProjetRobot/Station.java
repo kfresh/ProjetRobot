1c76edae245bf009a4f177627505076b3538f8d9
SAID Abdoul Kader <kdr_star@hotmail.com>
2014-01-03 11:34:31 +0100

    début de la classe file d'attente crée

---
+++ packages/fr/up10/miage/ProjetRobot/Station.java
@@ -0,0 +1,11 @@
+package fr.up10.miage.ProjetRobot;
+
+public class Station extends Thread{
+	private Robot[] FiledAttente ;
+	
+	public Station(){
+		Robot[] FiledAttente = new Robot[3];
+		
+	}
+	
+}
